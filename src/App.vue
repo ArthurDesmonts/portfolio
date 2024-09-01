475d2b6529c854e6114927f5eb96361f33023fd6
arthur <arthur.desmonts@gmail.com>
2024-09-01 19:15:56 +0200

    Add Nav bar + setting theme switch

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+import NavBar from "@/components/NavBar.vue";
+
+const isDarkTheme = ref(false);
+
+const toggleTheme = () => {
+  isDarkTheme.value = !isDarkTheme.value;
+  if (isDarkTheme.value) {
+    document.body.classList.add('dark-theme');
+  } else {
+    document.body.classList.remove('dark-theme');
+  }
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <NavBar></NavBar>
+  <h1>Arthur Desmonts, développeur Full-Stack.</h1>
+  <p>Diplômé d'un BUT Informatique (BAC +3)</p>
+  <button>Me Contacter</button>
+  <button @click="toggleTheme">Theme sombre</button>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
+/* CSS */
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+<ul>
+  <li><MyIcon>#</MyIcon> Accueil</li>
+  <li><MyIcon>#</MyIcon> Qui suis-je?</li>
+  <li><MyIcon>#</MyIcon> Mes Projets</li>
+  <li><MyIcon>#</MyIcon> Me Contacter</li>
+</ul>
+</template>
+
+<style scoped>
+/* CSS */
+/*  MyIcon purple color car */
+MyIcon {
+  color: purple;
+}
+</style>
