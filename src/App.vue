5d2ff4d791210866d0813200983cd1a22ac6a2d0
arthur <arthur.desmonts@gmail.com>
2024-09-05 17:40:50 +0200

    Cleaning global render + refactor of some parts of the project

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,33 @@ import NavBar from "@/components/NavBar.vue";
 </script>
 
 <template>
+
   <div class="container">
     <NavBar></NavBar>
-    <h1>Arthur Desmonts, développeur Full-Stack.</h1>
-    <p>Diplômé d'un BUT Informatique (BAC +3)</p>
-    <h2><MyIcon>#</MyIcon> Qui suis-je?</h2>
-    <p>Bonjour, Je suis Arthur.
+    <div class="HeadLine">
+      <h1>Arthur Desmonts, <MyIcon>développeur Full-Stack.</MyIcon></h1>
+      <p class="subTitle">Diplômé d'un BUT Informatique (BAC +3) - parcours
+        réalisation d'applications :
+        conception, développement,
+        validation</p>
+    </div>
+    <hr class="separateur">
+    <div class="presentation">
+      <h2><MyIcon>#</MyIcon> Qui suis-je?</h2>
+      <p class="justify-content">Bonjour, Je suis Arthur.
 
-      Je suis alternant en développement mobile, passionné de développement. J'ai obtenu un but informatique à l'IUT Grand Ouest Ifs. Durant mon cursus, j'ai pu acquérir de nombreuses compétences en développement, notamment en Web, en base de données et en programmation orientée objet.
+        Je suis alternant en développement mobile, passionné de développement. J'ai obtenu un but informatique à l'IUT Grand Ouest Ifs. Durant mon cursus, j'ai pu acquérir de nombreuses compétences en développement, notamment en Web, en base de données et en programmation orientée objet.
 
-      Ce que j'aime à travers le développement, c'est avant tout l'élaboration de solutions créatives résultant d'une contrainte. J'aime me creuser la tête pour parvenir à atteindre mes objectifs. Je suis enthousiaste et adore travailler avec des personnes passionnées.
+        Ce que j'aime à travers le développement, c'est avant tout l'élaboration de solutions créatives résultant d'une contrainte. J'aime me creuser la tête pour parvenir à atteindre mes objectifs. Je suis enthousiaste et adore travailler avec des personnes passionnées.
 
-    </p>
-    <h2><MyIcon>#</MyIcon> Me Contacter</h2>
-    <p>Je suis intéressé par des opportunités full remote. Cependant, si vous avez d'autres demandes ou questions, n'hésitez pas à me contacter.</p>
-    <button>Me Contacter</button>
+      </p>
+    </div>
+    <hr class="separateur">
+    <div class="contact">
+      <h2><MyIcon>#</MyIcon> Me Contacter</h2>
+      <p class="justify-content">Je suis intéressé par des opportunités full remote. Cependant, si vous avez d'autres demandes ou questions, n'hésitez pas à me contacter.</p>
+      <button class="innerParagraphButton">Me Contacter</button>
+    </div>
   </div>
 </template>
 
@@ -29,9 +41,66 @@ MyIcon {
 }
 
 .container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position : relative;
+  padding: 10em 1em 0 1em;
+}
+
+.HeadLine {
+  margin-left: 2em;
+}
+
+.justify-content {
+  text-align: justify;
+  padding : 1em;
+}
+
+.presentation {
+  margin-top: 2em;
+  margin-right: 60%;
+  margin-left: 2em;
+}
+
+.contact {
+  margin-top: 2em;
+  margin-left: 60%;
+  margin-right: 2em;
+}
+
+.separateur {
+  margin-top: 2em;
+  margin-left: 5em;
+  margin-right: 5em;
+  border: 1px solid #c16ed2;
+}
+
+.innerParagraphButton {
+  margin : 1em;
+}
+
+.subTitle {
+  font-size: 1em;
+  font-style: italic;
+}
+
+@media (max-width: 768px) {
+  .container {
+    padding: 1em;
+  }
+
+  .HeadLine {
+    margin-left: 0;
+    text-align: center;
+  }
+
+  .presentation {
+    margin: 0;
+  }
+
+  .contact {
+    margin: 0;
+  }
+
+  .separateur {
+    margin: 1em;
+  }
 }
 </style>
