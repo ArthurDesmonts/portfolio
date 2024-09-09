64f6942945b9fdc4f72195bc111f2ccd7a9ea003
arthur <arthur.desmonts@gmail.com>
2024-09-09 17:34:34 +0200

    add circle behind picture

--- src/components/ProfilPicComponent.vue
+++ src/components/ProfilPicComponent.vue
@@ -3,18 +3,28 @@
 </script>
 
 <template>
-  <img class="profilPic" src="@/assets/profilPic.png" alt="profilPicture">
+  <div class="circle">
+    <img class="profilPic" src="@/assets/profilPic.png" alt="profilPicture">
+  </div>
 </template>
 
 <style scoped>
+.circle {
+  width: 420px;
+  height: 420px;
+  border-radius: 300px 210px 0 0;
+  background-color: #c16ed2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 15%;
+}
 
 .profilPic {
   width: auto;
   height: 400px;
-  border-radius: 10%;
-  border: red 2px solid;
   object-fit: cover;
-  margin-right: 15%;
+  border-bottom-left-radius: 210px;
+  border-bottom-right-radius: 300px;
 }
-
 </style>
