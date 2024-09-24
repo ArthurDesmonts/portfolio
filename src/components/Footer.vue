c7adf0f3930ed11554c078f4ba11aeef9892908d
arthur <arthur.desmonts@gmail.com>
2024-09-24 11:05:06 +0200

    add content to footer + redirect to github

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,27 @@
 <script setup>
 
+function GithubRedirect() {
+  window.location.href="https://github.com/ArthurDesmonts";
+}
 </script>
 
 <template>
-
+  <div class="footer">
+    <p>Créé par Arthur Desmonts. 2024</p>
+    <p>Repository entièrement consultable sur mon GitHub : </p>
+    <img class="logo" src="@/assets/GitHub.png" alt="Github Icon" @click="GithubRedirect()">
+  </div>
 </template>
 
 <style scoped>
 
+.logo {
+  width: 75px;
+  height: 50px;
+}
+
+.logo:hover {
+  transform: scale(1.1);
+  cursor: pointer;
+}
 </style>
