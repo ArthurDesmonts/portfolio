5fd9a0c209f17b485f651b06593979bce11318b6
arthur <arthur.desmonts@gmail.com>
2024-09-25 11:37:48 +0200

    update footer content (add linkedin)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,11 @@
 function GithubRedirect() {
   window.location.href="https://github.com/ArthurDesmonts";
 }
+
+function redirectToLinkedIn() {
+  window.location.href="https://www.linkedin.com/in/arthur-desmonts-53a78b259/";
+}
+
 </script>
 
 <template>
@@ -10,6 +15,7 @@ function GithubRedirect() {
     <p>Créé par Arthur Desmonts. 2024</p>
     <p>Repository entièrement consultable sur mon GitHub : </p>
     <img class="logo" src="@/assets/GitHub.png" alt="Github Icon" @click="GithubRedirect()">
+    <img class="logo autoScale" src="@/assets/linkedin-Logo.png" alt="LinkedIn Icon" @click="redirectToLinkedIn()">
   </div>
 </template>
 
@@ -23,5 +29,10 @@ function GithubRedirect() {
 .logo:hover {
   transform: scale(1.1);
   cursor: pointer;
+}
+
+.autoScale {
+  width: 50px;
+  height: 50px;
 }
 </style>
