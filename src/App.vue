3a7d43fa64b04edf878a5748585ae70783297d95
arthur <arthur.desmonts@gmail.com>
2024-10-12 14:14:03 +0200

    update text content

--- src/App.vue
+++ src/App.vue
@@ -76,39 +76,39 @@ onMounted(() => {
       <div class="description-split">
         <p class="justify-content descriptionBox no-after appears-left">Bonjour, je m'appelle Arthur.
 
-          Je suis développeur Web orienté <span class="icon">Full-Stack</span>, avec une préférence pour le <span class="icon">Front-End</span>. J'ai obtenu un BUT informatique à l'IUT Grand Ouest Ifs. Durant mon cursus, j'ai pu acquérir de nombreuses compétences en développement, notamment en <span class="icon">Web</span>, en <span class="icon">bases de données</span>
-          et dans le domaine du <span class="icon">développement applicatif</span>.
+          Je suis développeur Web orienté <span class="icon">Full-Stack</span>, avec une préférence pour le <span class="icon">Front-End</span>. J'ai obtenu un BUT informatique à l'IUT Grand Ouest Ifs. Pendant mon cursus, j'ai pu acquérir de nombreuses compétences en développement, principalement en <span class="icon">Web</span>, en <span class="icon">bases de données</span>
+          ainsi qu'en <span class="icon">applicatif</span>.
 
-          J'ai perfectionné ces compétences au cours d'<span class="icon">une année d'alternance</span> dans une équipe de développeurs mobiles. J'ai pu travailler aussi bien sur <span class="icon">Android</span> que sur <span class="icon">iOS</span>. J'ai également eu l'occasion de contribuer à des projets de développement Web, plus particulièrement sur un long projet en <span class="icon">Vue.js</span>.
+          J'ai perfectionné ces compétences au cours d'<span class="icon">une année d'alternance</span> dans une petite équipe de développeurs mobiles. En leur compagnie, j'ai pu travailler aussi bien sur <span class="icon">Android</span> que sur <span class="icon">iOS</span>. Enfin, l'occasion s'est présentée de contribuer à des projets de développement Web, plus particulièrement sur un large projet <span class="icon">Vue.js</span>.
         </p>
         <p class="justify-content descriptionBox no-before appears-right">
-          Durant cette année, j'ai également manipulé plusieurs librairies d'<span class="icon">imagerie</span>, telles qu'<span class="icon">OpenCV</span>, <span class="icon">MLKit</span> ou encore <span class="icon">Apple Vision</span>. Enfin, j'ai clôturé cette expérience avec un projet utilisant de l'<span class="icon">OCR</span> (reconnaissance optique de caractères) pour développer différents algorithmes de reconnaissance.
+          Pendant cette année, j'ai également manipulé plusieurs librairies d'<span class="icon">imagerie</span>, telles qu'<span class="icon">OpenCV</span>, <span class="icon">MLKit</span> ou encore <span class="icon">Apple Vision</span>. Cela m'a permis de clôturer cette expérience avec un projet basé sur de l'<span class="icon">OCR</span> (reconnaissance optique de caractères) pour développer différents algorithmes de reconnaissance.
           Ce que j'aime dans le développement, c'est avant tout l'élaboration de solutions créatives.
-          Je suis <span class="icon">enthousiaste</span> et j'adore travailler avec des personnes passionnées. Je pense être parfaitement autonome dans ma façon de travailler, même si j'apprécie les retours sur ce que je peux améliorer.
+          Je suis <span class="icon">enthousiaste</span> et j'adore travailler avec des personnes passionnées. L’autonomie n’est pas un problème pour moi, même si j'apprécie les retours sur ce que je peux améliorer.
         </p>
       </div>
     </div>
     <hr id="project" class="separateur">
     <div class="presentation appears-top">
       <h2><span class="icon">#</span> Mes projets</h2>
-      <p class="justify-content descriptionBox">Malheureusement, je n'ai pas utilisé mon compte GitHub pour mes projets professionnels. De même, la plupart de mes répertoires universitaires étaient sur le serveur Git de l'université. Cependant, je peux néanmoins vous décrire en détail ceux que je trouve <span class="icon">les plus aboutis et représentatifs de mes compétences actuelles</span>.</p>
+      <p class="justify-content descriptionBox">Malheureusement, je n'ai pas utilisé mon compte GitHub pour mes projets professionnels. Cependant, je peux néanmoins vous décrire en détail ceux que je trouve <span class="icon">les plus aboutis et représentatifs de mes compétences actuelles</span>.</p>
       <div class="card-alignement">
         <div class="inner-text-card-align">
           <p class="vertical-align">IMAGERIE</p>
           <Card @expand-card="handleCardExpansion($event)" id="1" titleText="imagerie" html="Création d'un <span class='underline'>scanner de document</span> à l'aide d'un smartphone. Suppression <span class='underline'>automatique</span> de l'arrière-plan." image="openCv.png"
                 subTitle="Utilisation de <span class='icon'>OpenCV</span>"
                 first-block="Pour être plus précis, j'ai réalisé un détourage automatique à partir de la caméra d'un smartphone. C'est-à-dire que sur une photo, j'ai utilisé les différentes méthodes que propose OpenCV pour faire des traitements optiques.
-                Cela dans le but de faire gagner du temps à l'utilisateur qui doit prendre de nombreuses photos tout au long de la journée (contexte de déplacement avec durée limitée), mais également de confort (suppression d'une tâche manuelle répétitive)."
+                Cela, dans le but de faire gagner du temps à l'utilisateur qui doit prendre de nombreuses photos tout au long de la journée (contexte de déplacement avec durée limitée), mais également pour une démarche de confort (suppression d'une tâche manuelle répétitive)."
                 second-block="<span class='underline'>J'ai notamment axé mon développement sur les points suivants :</span>"
                 listOfSkills="Développement de l'interface utilisateur - Création de l'algorithme principal - Optimisation de l'algorithme, amélioration des performances - Conversion vers iOS (l'application métier étant en natif)"
-                third-block="Comme dit précédemment, je ne peux pas fournir de preuve physique de ce projet, appartenant à mon ancienne entreprise. Néanmoins, je peux conclure sur le fait que ce projet fut ma première expérience en terme de projet professionnel avec des exigences. J'ai fait face à un algorithme assez complexe vis-à-vis des exigences imposées. Enfin, j'ai eu un réel aperçu de la résolution de bugs et de l'optimisation de performances."></Card>
+                third-block="Comme dit précédemment, je ne peux pas fournir de preuve physique de ce projet, appartenant à mon ancienne entreprise. Néanmoins, je peux conclure sur le fait que ce projet fut ma première expérience en terme de projet professionnel avec des exigences. J'ai fait face à un algorithme complexe vis-à-vis des exigences imposées. Enfin, j'ai eu un réel aperçu de la résolution de bugs et de l'optimisation de performances."></Card>
         </div>
         <div class="inner-text-card-align">
           <p class="vertical-align">OCR</p>
           <Card @expand-card="handleCardExpansion($event)" id="2" titleText="ocr" html="Utilisation d'un <span class='underline'>OCR</span> sur des documents scannés, <span class='underline'>interprétation</span> des résultats, <span class='underline'>algorithme de reconnaissance des erreurs</span>." image="mlkit.png"
                 subTitle = "Utilisation de <span class='icon'>MLKit</span> et <span class='icon'>Apple Vision</span>"
                 first-block="J'ai travaillé sur un projet de reconnaissance de caractères. J'ai utilisé MLKit et Apple Vision pour interpréter des documents scannés. L'objectif de ce projet était dans un premier temps de rechercher les API d'OCR les plus performantes, fiables, maintenanables, et enfin adaptables sur les deux plateformes (iOS et Android). MLKIT étant une librairie Google, j'ai dû également me pencher sur Apple Vision pour la partie iOS.
-                Une fois mes recherches effectuées, j'ai, dans un premier temps, ajusté à l'aide de l'API la réception des caractères sur l'image et inséré notamment différentes méthodes de traitement pour améliorer la qualité de la reconnaissance, telles qu'un score de confiance ou encore d'isolement des mots. Enfin, j'ai développé un algorithme de reconnaissance des erreurs, pour permettre de ne conserver qu'une partie des éléments récupérés."
+                Une fois mes recherches effectuées, j'ai dans un premier temps ajusté à l'aide de l'API, la réception des caractères sur l'image. Puis, j'ai implémenté différentes méthodes de traitement pour améliorer la qualité de la reconnaissance, telles qu'un score de confiance ou encore un score d'isolement des mots. Enfin, viens le développement d'un algorithme de reconnaissance des erreurs, pour ne conserver qu'une partie des éléments récupérés."
                 second-block="<span class='underline'>Dans la globalité, j'ai pu travailler sur les points suivants :</span>"
                 listOfSkills="Recherche des API les plus performantes - Développement de l'interface utilisateur - Création de l'algorithme principal - Optimisation de l'algorithme, amélioration des performances - Algorithme de détection des erreurs (instauration de contextes) - Conversion vers iOS (l'application métier étant en natif)"
                 third-block="Ce projet a été une première en termes de recherche d'API et de développement d'algorithmes de reconnaissance. J'ai pu découvrir les différentes méthodes de traitement de texte et de la gestion des contextes. Enfin, j'ai réellement pu être autonome sur ce projet en ce qui concerne la recherche de solutions et la mise en place de ces dernières."
@@ -118,10 +118,10 @@ onMounted(() => {
           <p class="vertical-align">VUE.JS</p>
           <Card @expand-card="handleCardExpansion($event)" id="3" titleText="vue.js" html="Élaboration <span class='underline'>d'interfaces</span>, test des limites du framework, rédaction d'une documentation complète pour des novices." image="logo.svg"
                 subTitle="Développement d'interfaces avec <span class='icon'>Vue.js</span>"
-                first-block="Ce projet de développement frontend en Vue.js avait pour but de me laisser carte blanche quant à l'apprentissage de Vue3. L'objectif était de me familiariser avec le framework, de tester ses limites, et de rédiger une documentation complète pour les développeurs de l'équipe qui ne l'avaient jamais manipulé et qui, de plus, ne sont pas familiers avec JavaScript. Tout cela dans le but de juger si le framework serait une bonne opportunité pour une migration de l'application Web actuelle.
+                first-block="Ce projet de développement frontend en Vue.js avait pour but de me laisser carte blanche quant à l'apprentissage de Vue3. L'objectif était de me familiariser avec le framework, de tester ses limites, et de rédiger une documentation complète pour les développeurs de l'équipe qui ne l'avaient jamais manipulé et qui, de plus, ne sont pas familiers avec JavaScript. Tout cela avec pour ocjectif de juger si le framework serait une bonne opportunité pour une migration de l'application Web actuelle.
                 J'ai donc développé plusieurs interfaces en utilisant les différentes fonctionnalités de Vue3, mais également, j'ai testé les différentes façons de remplacer les fonctionnalités de l'application actuelle, majoritairement liées à la création de formulaires dynamiques ou encore aux appels AJAX."
                 second-block="<span class='underline'>Je peux synthétiser mon travail sur les points suivants :</span>"
-                list-of-skills="Développement de formulaires - Développement d'une SPA entièrement dynamique - Élaboration d'une API REST pour simuler des appels de formulaire sur le serveur - Test des limites du framework - Gestion du déploiement - Rédaction de la documentation"
+                list-of-skills="Développement de formulaires - Développement d'une SPA entièrement dynamique - Élaboration d'une API REST pour simuler des appels de formulaire sur le serveur (Spark : serveur, Axios : client) - Test des limites du framework - Gestion du déploiement - Rédaction de la documentation"
                 third-block="Ce projet fut ma première expérience de développement Web dans le milieu professionnel. J'ai pu découvrir les différentes méthodes de développement frontend. Enfin, l'utilisation de Vue3 m'a permis de mieux connaitre les frameworks JavaScript, et Vue en particulier me donne envie de continuer dans cette voix."
           ></Card>
         </div>
@@ -139,7 +139,7 @@ onMounted(() => {
         </div>
         <div id="contact" class="contact appears-right">
           <h2><span class="icon">#</span> Me Contacter</h2>
-          <p class="justify-content">Je suis actuellement en <span class="icon">recherche d'un premier emploi</span> dans le domaine du développement <span class="icon">Front-End</span>. Je suis ouvert à toutes les propositions que vous pouvez me présenter aux alentours de <span class="icon">Caen</span> par mail. Je suis intéressé par des opportunités <span class="icon">full remote</span>. Cependant, si vous avez d'autres demandes ou questions, n'hésitez pas à me contacter.</p>
+          <p class="justify-content">Je suis actuellement en <span class="icon">recherche d'un premier emploi</span> dans le domaine du développement <span class="icon">Full-Stack</span> ou  <span class="icon">Front-end</span>. Je suis ouvert à toutes les propositions que vous pouvez me présenter aux alentours de <span class="icon">Caen</span> par mail. Je suis intéressé par des opportunités <span class="icon">full remote</span>. Cependant, si vous avez d'autres demandes ou questions, n'hésitez pas à me contacter.</p>
           <div class="mail-message">
             <div class="mail-succes-listener">
               <button class="innerParagraphButton contact" @click="copyToClipboard">Contacter</button>
