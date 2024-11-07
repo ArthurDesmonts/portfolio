f4586ab70bc23aa37ae8430bc036ea8a6fe01c8e
arthur <arthur.desmonts@gmail.com>
2024-11-07 15:58:46 +0100

    position of the icons, redirect to code or app, visibility limit

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -74,6 +74,8 @@ const closeCard = () => {
     }
   }, 400);
 };
+
+const displayLinks = props.gitHubLink && props.link;
 </script>
 
 <template>
@@ -94,6 +96,16 @@ const closeCard = () => {
           <li v-for="skill in skillsList" :key="skill">{{ skill }}</li>
         </ul>
         <p class="justify-content" v-html="thirdBlock"></p>
+        <div v-if="displayLinks" class="link-button">
+          <a :href="gitHubLink" target="_blank" class="innerParagraphIcon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.385.6.11.82-.26.82-.577v-2.165c-3.338.726-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.757-1.333-1.757-1.09-.745.083-.73.083-.73 1.205.085 1.84 1.24 1.84 1.24 1.07 1.835 2.807 1.305 3.492.998.108-.775.418-1.305.76-1.605-2.665-.305-5.466-1.335-5.466-5.93 0-1.31.467-2.38 1.235-3.22-.125-.305-.535-1.54.115-3.205 0 0 1.005-.32 3.3 1.23.96-.267 1.98-.4 3-.405 1.02.005 2.04.138 3 .405 2.28-1.55 3.285-1.23 3.285-1.23.655 1.665.245 2.9.12 3.205.77.84 1.235 1.91 1.235 3.22 0 4.61-2.805 5.62-5.475 5.92.43.37.81 1.1.81 2.22v3.285c0 .32.215.695.825.575C20.565 21.795 24 17.295 24 12c0-6.63-5.37-12-12-12z"/>
+            </svg>
+          </a>
+          <a :href="link" target="_blank" class="innerParagraphImage">
+            <img :src="imageUrl" alt="Typefaster Logo" width="24" height="24">
+          </a>
+        </div>
         <div class="closure-button-container">
           <button class="innerParagraphButton closure-button" @click="closeCard">
             <span class="material-icons">arrow_upward</span>
@@ -197,6 +209,24 @@ const closeCard = () => {
   display: flex;
   flex-direction: column;
   align-items: flex-end;
+}
+
+.link-button {
+  display: flex;
+  flex-direction: row;
+  gap : 2em;
+  margin-top: 1em;
+  margin-left: auto;
+  padding-right: 1em;
+}
+
+.innerParagraphIcon {
+  fill: var(--text-color);
+}
+
+.innerParagraphImage {
+  width: 24px;
+  height: 24px;
 }
 
 @keyframes expand {
